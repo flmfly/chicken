5ad37448e82ba215ae975e89d3034084092f2f2f
lizhefeng <lizhefeng>
2016-06-24 08:37:13 +0800

    add config page

---
+++ app/pages/account-manager/account-manager.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+/*
+  Generated class for the AccountManagerPage page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  templateUrl: 'build/pages/account-manager/account-manager.html',
+})
+export class AccountManagerPage {
+  constructor(private nav: NavController) {}
+
+  onPageWillEnter() {
+    document.getElementsByTagName("ion-navbar-section")[0]["style"].display = "block";
+  }
+
+//show nav bar when we leave the page
+  onPageDidLeave() {
+    document.getElementsByTagName("ion-navbar-section")[0]["style"].display = "none";
+  }
+
+  openNavDetailsPage(item: any){
+
+  }
+}
